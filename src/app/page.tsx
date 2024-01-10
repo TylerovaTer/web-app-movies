c94e92eb1927d9d001cd1aae7231ff42889d0226
TylerovaTer <tereza.tylerova@seznam.cz>
2024-01-10 19:39:20 +0100

    logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
+"use client"
 import { Card } from "@/components/card/card";
-import * as moviesObject from "@/data/movies.json";
+import { SortButton } from "@/components/sort-button";
+import data from "@/data/movies.json";
 import { FilmType } from "@/data/types";
+import { useState } from "react";
 
 export default function Home() {
-  const movies = moviesObject.movies as FilmType[]
+  const [sortedFromNewest, setSortedFromNewest] = useState<boolean>(true)
+  const [movieSorted, setMoviesSorted] = useState(data.movies as FilmType[])
+
+  const sorting = () => {
+    let sortedArray: FilmType[] = [...movieSorted]
+    if (sortedFromNewest) {
+      sortedArray.sort((a, b) => parseInt(b.episode_number) - parseInt(a.episode_number))
+      setSortedFromNewest(false)
+    } else {
+      sortedArray.sort((a, b) => parseInt(a.episode_number) - parseInt(b.episode_number))
+      setSortedFromNewest(true)
+    }
+    setMoviesSorted(sortedArray)
+  }
+
   return (
     <>
-      {movies.map((item, index) => {
+    <SortButton handleSorting={sorting} latest={sortedFromNewest}/>
+      {movieSorted.map((item, index) => {
         return (
           <Card film={item} key={index} />
         )
